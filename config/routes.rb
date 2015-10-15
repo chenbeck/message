e020db786ca2c3104e78d83077e53075c8249071
Beck Chen <chenbeck@gmail.com>
2015-10-15 15:27:56 +0800

    basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :topics
+
+  get 'welcome' => 'topics#welcome'
+  root 'topics#index' #設定首頁是topics
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
